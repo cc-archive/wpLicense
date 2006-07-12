9c3d4bf6b278109f8d956056f8e80041aa430353
nyergler <nyergler@committer.creativecommons.org>
2006-07-12 16:50:29 +0000

    [project @ not-simplexml] Converted XML parsing and extraction from SimpleXml (PHP5-only) to MiniXML (bundled PHP4/PHP5 library); added MiniXML support library.
    Original author: Nathan R. Yergler <nathan@yergler.net>
    Date: 2005-07-11 22:40:33

--- wpLicense/ccwsclient.php
+++ wpLicense/ccwsclient.php
@@ -1,29 +1,40 @@
 <?php
 
-$WS_ROOT = "http://api.creativecommons.org/rest/1.0";
+require_once(dirname(__FILE__).'/minixml.inc.php');
+
+$WS_ROOT = "http://api.creativecommons.org/rest/1.0/";
 
 function licenseClasses() {
-   $uri="http://api.creativecommons.org/rest/1.0/";
+   global $WS_ROOT;
+   
+   $l_classes = array();
 
    // retrieve the license 
-   $cobj=curl_init($uri);
-   curl_setopt($cobj,CURLOPT_RETURNTRANSFER,1);
+   $cobj=curl_init($WS_ROOT);
+   curl_setopt($cobj, CURLOPT_RETURNTRANSFER, 1);
    $xml=curl_exec($cobj);
    curl_close($cobj);
 
    // parse the classes into a hash
-   $xmldoc = simplexml_load_string($xml);
-   $l_classes = array();
+   $xmlDoc = new MiniXMLDoc();
+   $xmlDoc->fromString($xml);
 
-   foreach ($xmldoc->xpath('//license') as $license) {
-      $l_classes[(string) $license['id']] = htmlentities((string) $license);
-   } // foreach
+   $root =& $xmlDoc->getRoot()->getElement('licenses');
+   $licenses =& $root->getAllChildren('license');
+
+   foreach ($licenses as $l) {
+      $l_classes[strval($l->attribute('id'))] = strval($l->getValue());
+   }
 
    return $l_classes;
+
 } // licenseClasses
 
 function licenseQuestions($lclass) {
-   $uri = "http://api.creativecommons.org/rest/1.0/license/" . $lclass . "/";
+   global $WS_ROOT;
+
+   $uri = $WS_ROOT.'license/'.$lclass."/";
+   $questions = array();
 
    // retrieve the license 
    $cobj=curl_init($uri);
@@ -32,20 +43,23 @@ function licenseQuestions($lclass) {
    curl_close($cobj);
  
    // parse the classes into a hash
-   $xmldoc = simplexml_load_string($xml);
-   $questions = array();
+   $xmlDoc = new MiniXMLDoc();
+   $xmlDoc->fromString($xml);
 
-   foreach ($xmldoc->xpath('//field') as $field) {
-    $f_id = (string) $field['id'];
+   $root =& $xmlDoc->getRoot()->getElement('licenseclass');
+   $fields =& $root->getAllChildren('field');
+
+   foreach ($fields as $field) {
+    $f_id = strval($field->attribute('id'));
     $questions[$f_id] = array();
 
-    $questions[$f_id]['label'] = (string) $field->label[0];
-    $questions[$f_id]['description'] = (string) $field->description[0];
-    $questions[$f_id]['type'] = (string) $field->type[0];
+    $questions[$f_id]['label'] = strval($field->getElement('label')->getValue());
+    $questions[$f_id]['description'] = strval($field->getElement('description')->getValue());
+    $questions[$f_id]['type'] = strval($field->getElement('type')->getValue());
     $questions[$f_id]['options'] = array();
 
-    foreach ($field->enum as $enum) {
-       $questions[$f_id]['options'][(string)$enum['id']] = (string)$enum->label[0];
+    foreach ($field->getAllChildren('enum') as $enum) {
+       $questions[$f_id]['options'][(string)$enum->attribute('id')] = (string)$enum->getElement('label')->getValue();
     } // for each enum
 
    } // foreach
@@ -54,6 +68,9 @@ function licenseQuestions($lclass) {
 } // licenseQuestions
 
 function issueLicense($lic_class, $answers) {
+
+   global $WS_ROOT;
+   $result = array();
 
    // assemble the answers XML fragment
    $answers_xml = "<answers><license-" . $lic_class . ">";
@@ -65,19 +82,29 @@ function issueLicense($lic_class, $answers) {
    $answers_xml .= "</license-" . $lic_class . "></answers>";
 
    // make the web service request
-   $uri = "http://api.creativecommons.org/rest/license/" . $lic_class . "/issue?answers=" . urlencode($answers_xml);
+   $uri = $WS_ROOT."license/" . $lic_class . "/issue?answers=" . urlencode($answers_xml);
    $cobj=curl_init($uri);
    curl_setopt($cobj, CURLOPT_RETURNTRANSFER, true);
    $xml=curl_exec($cobj);
    curl_close($cobj);
 
-   $xmldoc = simplexml_load_string($xml);
-   $result = array();
+   // extract the license information
+   $xmlDoc = new MiniXMLDoc();
+   $xmlDoc->fromString($xml);
 
-   $result["uri"] = (string)$xmldoc->{"license-uri"};
-   $result["name"] = (string)$xmldoc->{"license-name"};
-   $result["rdf"] = (string)$xmldoc->rdf->asXML();
-   $result["html"] = (string)$xmldoc->html->asXML();
+   $root =& $xmlDoc->getRoot()->getElement('result');
+
+   $result["uri"] = strval($root->getElement('license-uri')->getValue());
+   $result["name"] = strval($root->getElement('license-name')->getValue());
+   $result["rdf"] = $root->getElement('rdf')->toString();
+   $result["html"] = $root->getElement('html')->toString();
+
+   // $xmldoc = simplexml_load_string($xml);
+
+   //$result["uri"] = (string)$xmldoc->{"license-uri"};
+   //$result["name"] = (string)$xmldoc->{"license-name"};
+   //$result["rdf"] = (string)$xmldoc->rdf->asXML();
+   //$result["html"] = (string)$xmldoc->html->asXML();
 
    return $result;
 } // issueLicense
