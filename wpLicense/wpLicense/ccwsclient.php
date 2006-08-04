9699309b6f901083aa0a7e275c8ac8c380fea92f
nyergler <nyergler@committer.creativecommons.org>
2006-08-04 21:30:36 +0000

    Removing crufted local-xml-fallback support.

--- wpLicense/wpLicense/ccwsclient.php
+++ wpLicense/wpLicense/ccwsclient.php
@@ -3,7 +3,6 @@
 require_once(dirname(__FILE__).'/minixml.inc.php');
 
 $WS_ROOT = "http://api.creativecommons.org/rest/1.5/";
-$FS_ROOT = dirname(__FILE__).'/static_xml/';
 
 function fopenEnabled() {
 
@@ -21,66 +20,16 @@ function retrieveFile($path) {
    // if unavailable, from the local static cache
 
    global $WS_ROOT;
-   global $FS_ROOT;
 
-if (fopenEnabled()) {
    // try to retrieve the information from the CC web services
-      $result = file_get_contents($WS_ROOT.$path);
+   $result = file_get_contents($WS_ROOT.$path);
   
-      if (!($result === FALSE)) {
-         return $result;
-      }
-} else {
-   // fallback to filesystem cache
-   if (!$path) {
-      $path = "classes";
+   if (!($result === FALSE)) {
+      return $result;
    }
-
-   return file_get_contents($FS_ROOT.$path);
-}
 
 } // retrieveFile
 
-
-function localIssue($answers) {
-   // issue a license using a locally cached copy of the XSLT
-
-   global $FS_ROOT;
-
-   // switch behavior depending on PHP4 v PHP5
-   if (function_exists("xslt_process")) {
-      // PHP4 behavior
-
-      $xp = xslt_create() or die("Could not create XSLT processor");
-      $xslt_string = join("", file($FS_ROOT."/xslt/chooselicense.xsl"));
-   
-      $arg_buffer = array("/xml" => $answers, 
-                          "/xslt" => $xslt_string);
-
-      if($result = xslt_process($xp, "arg:/xml", "arg:/xslt", NULL, $arg_buffer))
-      {
-         // return result
-         return $result;
-      }   
-   } else {
-      // PHP 5 behavior
-
-      $xml = new DOMDocument;
-      $xml->loadXML($answers);// ('collection.xml');
-
-      $xsl = new DOMDocument;
-      $xsl->load($FS_ROOT."/xslt/chooselicense.xsl");
-
-      // Configure the transformer
-      $proc = new XSLTProcessor;
-      $proc->importStyleSheet($xsl); // attach the xsl rules
-
-      return $proc->transformToXML($xml);
-
-   } // php 5
-
-} // localIssue
-   
 function licenseClasses() {
    
    $l_classes = array();
@@ -165,14 +114,8 @@ function issueLicense($lic_class, $answers) {
    $answers_xml .= "</license-" . $lic_class . "></answers>";
 
    // make the web service request
-   $xml = FALSE;
-if (fopenEnabled()) {
-      $uri = $WS_ROOT."license/" . $lic_class . "/issue?answers=" . urlencode($answers_xml);
-      $xml = file_get_contents($uri);
-} else {
-   // check if remote retrieval failed and fall back to local if necessary
-     $xml = localIssue($answers_xml);
-   }
+   $uri = $WS_ROOT."license/" . $lic_class . "/issue?answers=" . urlencode($answers_xml);
+   $xml = file_get_contents($uri);
 
    // extract the license information
    $xmlDoc = new MiniXMLDoc();
