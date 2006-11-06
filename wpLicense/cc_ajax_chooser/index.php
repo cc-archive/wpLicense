99bd29d1321c50e103ad88045d2df58f63e7de2a
nyergler <nyergler@committer.creativecommons.org>
2006-11-06 13:58:06 +0000

    Sample use of selector.

---
+++ wpLicense/cc_ajax_chooser/index.php
@@ -0,0 +1,40 @@
+<?php
+     /* Creative Commons cc-ajax license chooser
+      * Sample Web Services Client
+      * copyright 2005-2006, Creative Commons, Nathan R. Yergler
+      *
+      * See docs/LICENSE for details.
+      */
+
+   require_once(dirname(__FILE__).'/html_ui.php');
+
+?>
+
+<html>
+  <head>
+    <title>Creative Commons AJAX License Chooser Demo</title>
+
+<!-- insert the Javascript include statements -->
+<?php scriptHeader($base='.'); ?>
+
+<style>
+th { 
+    white-space: nowrap; 
+}
+
+img {
+    display:none; 
+    float:right; 
+    margin:0px;
+    padding:0px;
+}
+
+</style>
+  </head>
+
+  <body>
+
+<?php licenseChooser($action='choose.php', $prefix=''); ?>
+
+  </body>
+</html>
